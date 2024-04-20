d2a10737f8a7b9d4e23ed55bbbba5d7b93ac5818
Andrew Zimmerman <azimmerman@pga.com>
2024-04-20 07:37:51 -0400

    refactor of the user controller and supporting functions and functions to mirgrate to mysql

--- express-backend/controllers/users.js
+++ express-backend/controllers/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 require('dotenv').config()
-const pool = require('../models/db')
+const { mysqlPool } = require('../models/db')
 
 // Functions
 const generateSalt = require('../functions/generateSalt')
@@ -20,11 +20,11 @@ router.get('/', async (req, res) => {
       B.wins,
       B.best_finish,
       B.low_score
-    FROM public."Users" A, public."User_Data" B
+    FROM \`major-fantasy-golf\`.Users A, \`major-fantasy-golf\`.User_Data B
     WHERE A.user_id = B.user_id;`
 
     try {
-      const response = await pool.query(getUsers)
+      const [response, metadata] = await mysqlPool.query(getUsers)
       if (response.error) res.status(500).send({response})
       else res.status(200).send(response)
     } catch (error) {
@@ -34,16 +34,15 @@ router.get('/', async (req, res) => {
 
 //GET LIST OF USERNAMES TO VALIDATE NEW USER ON FRONTEND
 router.get('/usernamelist', async (req, res) => {
-  const getUsers = `SELECT A.user_name, A.email FROM public."Users" A;`
+  const getUsers = `SELECT user_name, email FROM \`major-fantasy-golf\`.Users A;`
   try {
-    const response = await pool.query(getUsers)
+    const [response, metadata] = await mysqlPool.query(getUsers)
     if (response.error) res.status(500).send({response})
     else {
-      const { rows } = response
       // transform key-value pairs to just value
       let userNames = []
       let emails = []
-      rows.forEach(row => {
+      response.forEach(row => {
         // console.log(row)
         userNames.push(row['user_name'])
         emails.push(row['email'])
@@ -68,16 +67,15 @@ router.get('/:id', async (req, res) => {
       B.wins,
       B.best_finish,
       B.low_score
-    FROM public."Users" A, public."User_Data" B
+    FROM \`major-fantasy-golf\`.Users A, \`major-fantasy-golf\`.User_Data B
     WHERE A.user_id = B.user_id 
       AND A.user_name = '${id}';`
 
     try {
-      const response = await pool.query(getUser)
+      const [response, metadata] = await mysqlPool.query(getUser)
       if (response.error) res.status(500).send({response})
       else {
-        const { rows } = response
-        res.status(200).send(rows[0])
+        res.status(200).send(response)
       }
     } catch (error) {
       console.error(error)
@@ -94,22 +92,22 @@ router.post('/new', async (req, res) => {
   const { first_name, last_name, user_name, email, password } = req.body
 
   // Validate if user_name and email are unique
-  const checkUserName = `SELECT A.user_name FROM public."Users" A
-    WHERE A.user_name = '${user_name.toLowerCase()}'
-      OR A.email = '${email.toLowerCase()}'`
+  const checkUserName = `SELECT user_name FROM \`major-fantasy-golf\`.Users
+    WHERE user_name = '${user_name.toLowerCase()}'
+      OR email = '${email.toLowerCase()}'`
 
   try {
-    const userNameResponse = await pool.query(checkUserName)
+    const [userNameResponse, userNameMetadata] = await mysqlPool.query(checkUserName)
         
     if (!user_name || !email || !password) res.status(400).send('Invalid request - Required fields not entered')
-    else if (userNameResponse.rowCount > 0) res.status(400).send('Invalid request - Username or Email already exists')
+    else if (userNameResponse.length > 0) res.status(400).send('Invalid request - Username or Email already exists')
     else {
       salt = generateSalt(32)// function to generate salt
       let encryptedSalt = encryptVaule(salt) // function to encrypt the salt
       let passwordHash = hashValue(password + salt) // funtion to hash the password
 
       // Insert Query for new user
-      const createUser = `INSERT INTO public."Users" (first_name, last_name, user_name, email, salt, password_hash, created_at, updated_at)
+      const createUser = `INSERT INTO \`major-fantasy-golf\`.Users (first_name, last_name, user_name, email, salt, password_hash, created_at, updated_at)
         VALUES (${first_name ? `'${first_name}'` : undefined},
           ${last_name ? `'${last_name}'` : undefined},
           '${user_name.toLowerCase()}', 
@@ -119,22 +117,21 @@ router.post('/new', async (req, res) => {
           NOW(), 
           NOW());`
 
-      let response = await pool.query(createUser)
+      let [response, metadata] = await mysqlPool.query(createUser)
       if (response.error) res.status(500).send({response})
       else {
         // Query to get the user id 
-        const getUserId = `SELECT A.user_id FROM public."Users" A
-          WHERE A.user_name = '${user_name.toLowerCase()}';`
+        const getUserId = `SELECT user_id FROM \`major-fantasy-golf\`.Users
+          WHERE user_name = '${user_name.toLowerCase()}';`
 
-        response = await pool.query(getUserId)
+        [response, metadata] = await mysqlPool.query(getUserId)
         if (response.error) res.status(500).send({response})
         else {
-          const { rows } = response
           // Query to add record to the User Data Table
-          const userDataInsert = `INSERT INTO public."User_Data" (user_id, created_at, updated_at)
-            VALUES (${rows[0]["user_id"]}, NOW(), NOW());` 
+          const userDataInsert = `INSERT INTO \`major-fantasy-golf\`.User_Data (user_id, created_at, updated_at)
+            VALUES (${response[0]["user_id"]}, NOW(), NOW());` 
 
-            response = await pool.query(userDataInsert)
+            response = await mysqlPool.query(userDataInsert)
             if (response.error) res.status(500).send({response})
             // log in user
             else res.redirect(307, '/auth/')
@@ -156,13 +153,13 @@ router.put('/:id', async (req, res) => {
   // Validate if user_name and email are unique
   let rowCount = 0
   if (email || user_name) {
-    const checkUserName = `SELECT A.user_name FROM public."Users" A
-    WHERE (A.user_name = '${user_name ? user_name.toLowerCase() : ''}'  
-        OR A.email = '${email ? email.toLowerCase() : ''}')
-      AND A.user_id <> ${id};`
+    const checkUserName = `SELECT user_name FROM \`major-fantasy-golf\`.Users
+    WHERE (user_name = '${user_name ? user_name.toLowerCase() : ''}'  
+        OR email = '${email ? email.toLowerCase() : ''}')
+      AND user_id <> ${id};`
 
     try {
-      const userNameResponse = await pool.query(checkUserName)
+      const [userNameResponse, metadata] = await mysqlPool.query(checkUserName)
       rowCount = userNameResponse.rowCount
     } catch (error) {
       console.error(error)
@@ -173,7 +170,7 @@ router.put('/:id', async (req, res) => {
   // No update if email or username already exist
   if (rowCount > 0)  res.status(400).send('Invalid request - Username or Email already exists')
   // build the query 
-  let updateUser = `UPDATE public."Users"
+  let updateUser = `UPDATE \`major-fantasy-golf\`.Users
   SET updated_at = NOW()`
 
   if (first_name) updateUser = updateUser + `, first_name = '${first_name}'`
@@ -186,7 +183,7 @@ router.put('/:id', async (req, res) => {
   WHERE user_id = '${id}';`
 
   try {
-    const response = await pool.query(updateUser)
+    const [response, metadata] = await mysqlPool.query(updateUser)
     if (response.error) res.status(500).send(response)
     else res.status(200).send(response)
   } catch (error) {
@@ -201,13 +198,13 @@ router.put('/:username/role', async (req, res) => {
   const { username } = req.params
 
   // build the query 
-  let updateUserRole = `UPDATE public."Users"
+  let updateUserRole = `UPDATE \`major-fantasy-golf\`.Users
   SET updated_at = NOW(),
     role = '${role}'
   WHERE user_name = '${username.toLowerCase()}';`
 
   try {
-    const response = await pool.query(updateUserRole)
+    const [response, metadata] = await mysqlPool.query(updateUserRole)
     if (response.error) res.status(500).send({response})
     else res.status(200).send(response)
   } catch (error) {
@@ -225,18 +222,17 @@ router.put('/:user_id/password', async (req, res) => {
   else if (changePassword !== confirmPassword) res.status(400).send({msg: 'Passswords Do Not Match'})
   else {
     // Get salt for password hashing and hash for validation
-    const saltQuery = `SELECT A.salt, A.password_hash FROM public."Users" A
+    const saltQuery = `SELECT salt, password_hash FROM \`major-fantasy-golf\`.Users
       WHERE user_id = ${user_id}
       LIMIT 1;`
 
     try {
-      const response = await pool.query(saltQuery)
+      const [response, metadata] = await mysqlPool.query(saltQuery)
 
-      const { rows, rowCount, error } = response 
-      if (error) res.status(500).send({msg: 'Password Update Unsuccessful'})
-      else if (rowCount === 0) res.status(500).send({msg: 'Password Update Unsuccessful'})
+      if (response.error) res.status(500).send({msg: 'Password Update Unsuccessful'})
+      else if (response.length === 0) res.status(500).send({msg: 'Password Update Unsuccessful'})
       else {
-        const { salt, password_hash } = rows[0]
+        const { salt, password_hash } = response[0]
 
         // Validate current password is correct
         let decryptedSalt = decryptValue(salt) // decrypt salt value from the db
@@ -247,12 +243,12 @@ router.put('/:user_id/password', async (req, res) => {
         let passwordHash = hashValue(changePassword + decryptedSalt) // funtion to hash the new password
 
         // build new password query
-        let updateUserPassword = `UPDATE public."Users"
+        let updateUserPassword = `UPDATE \`major-fantasy-golf\`.Users
           SET updated_at = NOW(),
             password_hash = '${passwordHash}'
           WHERE user_id = ${user_id};`
 
-        const response = await pool.query(updateUserPassword)
+        const [response, metadata] = await mysqlPool.query(updateUserPassword)
         if (response.error) {res.status(500).send({msg: 'Password Update Unsuccessful'})}
         else res.status(200).send({msg: 'Password Update Successful'})
       }
@@ -278,19 +274,19 @@ router.delete('/:id', async (req, res) => {
   ]
 
   try {
-    pool.query('BEGIN')
+    // pool.query('BEGIN')
     tables.forEach(async (table) => {
-      let removeUser = `DELETE FROM public."${table}"
+      let removeUser = `DELETE FROM \`major-fantasy-golf\`.${table}
       WHERE user_id = ${id};`
 
-    await pool.query(removeUser)
+    await mysqlPool.query(removeUser)
     })
 
-    pool.query('COMMIT')
+    // pool.query('COMMIT')
     res.status(200).send('User Deleted')
   } catch (error) {
     console.error(error, 'rollback')
-    pool.query('ROLLBACK')
+    // pool.query('ROLLBACK')
     res.status(500).send(error)
   }
 })
